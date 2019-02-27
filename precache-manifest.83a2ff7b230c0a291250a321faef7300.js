0f6df1671c2b11e05f8967c19d2bf2db16d03a09
francisco1508 <francisco.ipn02@gmail.com>
2019-02-27 14:33:52 -0600

    Updates

---
+++ precache-manifest.83a2ff7b230c0a291250a321faef7300.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3529177762bbabf3741b6a3667274e8",
+    "url": "/eldventir_pages/static/media/Base.d3529177.svg"
+  },
+  {
+    "revision": "99d9a81b69dd9110f8da",
+    "url": "/eldventir_pages/static/css/main.6395f8b1.chunk.css"
+  },
+  {
+    "revision": "6993fc09523abb8f90c9",
+    "url": "/eldventir_pages/static/js/1.6993fc09.chunk.js"
+  },
+  {
+    "revision": "4cbf169290ddc6cae0b9",
+    "url": "/eldventir_pages/static/js/runtime~main.4cbf1692.js"
+  },
+  {
+    "revision": "63641e03bee0358dd351984df2c69140",
+    "url": "/eldventir_pages/static/media/Repisa.63641e03.svg"
+  },
+  {
+    "revision": "401c7200b3ec03520a6b0e6d744ab865",
+    "url": "/eldventir_pages/static/media/fondo-instrucciones.401c7200.png"
+  },
+  {
+    "revision": "a83da698ba4ab615b6084f434e998bf9",
+    "url": "/eldventir_pages/static/media/Personaje_balanza.a83da698.png"
+  },
+  {
+    "revision": "99d9a81b69dd9110f8da",
+    "url": "/eldventir_pages/static/js/main.99d9a81b.chunk.js"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/eldventir_pages/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/eldventir_pages/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "100b38fa184634fc89bd07a84453992c",
+    "url": "/eldventir_pages/static/media/Montserrat-Light.100b38fa.ttf"
+  },
+  {
+    "revision": "05289e866fe7e1e99d27a7a31f8d3b66",
+    "url": "/eldventir_pages/static/media/Trattatello.05289e86.ttf"
+  },
+  {
+    "revision": "5e442a446bbc541e6db7014215d6cb50",
+    "url": "/eldventir_pages/static/media/Fondo.5e442a44.jpg"
+  },
+  {
+    "revision": "8a07ff3faccdc86b07f701ea8224dc97",
+    "url": "/eldventir_pages/static/media/Balanza.8a07ff3f.svg"
+  },
+  {
+    "revision": "48c2919587083d0b89ec74752b34fa32",
+    "url": "/eldventir_pages/index.html"
+  }
+];
